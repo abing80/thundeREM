ec6abe6a12791a0353e25b0e8c3c3258105b8fe6
abing80 <andrea.bertolini80@gmail.com>
2024-05-17 17:34:08 +0200

    Update sounding_hodograph.R

--- R/sounding_hodograph.R
+++ R/sounding_hodograph.R
@@ -26,8 +26,8 @@
 #'   altitude = northplatte$altitude, max_speed = 40
 #' )
 #' title("North Platte - 3 July 1999, 00:00 UTC")
-sounding_hodograph = function(ws, wd, altitude, max_hght = 12000, max_speed = 25,
-                              lab_hghts = c(0, 1, 3, 6, 9, 12),
+sounding_hodograph = function(ws, wd, altitude, max_hght = 2250, max_speed = 25,
+                              lab_hghts = c(0, 0.5, 1, 1.5, 2),
                               close_par = TRUE, SRH_polygon = "03km_RM",
                               storm_motion = c(999, 999), ...) {
 
@@ -69,27 +69,6 @@ sounding_hodograph = function(ws, wd, altitude, max_hght = 12000, max_speed = 25
     xaxt = "n", yaxt = "n", frame.plot = FALSE
   )
 
-  if (SRH_polygon == "03km_RM") {
-    parametry2 = sounding_compute(seq(1000, 100, length.out = length(wd)),
-                                  altitude, seq(30, -60, length.out = length(wd)),
-                                  seq(20, -40, length.out = length(wd)),
-                                  wd, ws, storm_motion = storm_motion, accuracy = 3)
-    parametry3 = sounding_export(seq(1000, 100, length.out = length(wd)),
-                                 altitude, seq(30, -60, length.out = length(wd)),
-                                 seq(20, -40, length.out = length(wd)),
-                                 wd, ws, storm_motion = storm_motion)
-    RM_y = round(-parametry2[which(names(parametry2) == "Bunkers_RM_M")] * cos(parametry2[which(names(parametry2) == "Bunkers_RM_A")] * pi / 180), 2)
-    RM_x = round(-parametry2[which(names(parametry2) == "Bunkers_RM_M")] * sin(parametry2[which(names(parametry2) == "Bunkers_RM_A")] * pi / 180), 2) # LM_x = m_x1 - 7.5 * (sqrt( 1 / (1+slope2*slope2)))
-    uSRH = c(round(-parametry3$ws * 0.514444 * sin(parametry3$wd * pi / 180), 2)[(parametry3$altitude - parametry3$altitude[1]) >= 0 & (parametry3$altitude - parametry3$altitude[1]) <= 3000])
-    vSRH = c(round(-parametry3$ws * 0.514444 * cos(parametry3$wd * pi / 180), 2)[(parametry3$altitude - parametry3$altitude[1]) >= 0 & (parametry3$altitude - parametry3$altitude[1]) <= 3000])
-    for (i in 1:(1 + length(uSRH))) {
-      polygon(c(RM_x, uSRH[i:(i + 1)]),
-        c(RM_y, vSRH[i:(i + 1)]),
-        col = "red", border = NA
-      )
-    }
-  }
-
   if (SRH_polygon == "01km_RM") {
     parametry2 = sounding_compute(seq(1000, 100, length.out = length(wd)), altitude, seq(30, -60, length.out = length(wd)), seq(20, -40, length.out = length(wd)), wd, ws, storm_motion = storm_motion, accuracy = 3)
     parametry3 = sounding_export(seq(1000, 100, length.out = length(wd)), altitude, seq(30, -60, length.out = length(wd)), seq(20, -40, length.out = length(wd)), wd, ws, storm_motion = storm_motion)
@@ -112,21 +91,6 @@ sounding_hodograph = function(ws, wd, altitude, max_hght = 12000, max_speed = 25
     RM_x = round(-parametry2[which(names(parametry2) == "Bunkers_RM_M")] * sin(parametry2[which(names(parametry2) == "Bunkers_RM_A")] * pi / 180), 2) # LM_x = m_x1 - 7.5 * (sqrt( 1 / (1+slope2*slope2)))
     uSRH = c(round(-parametry3$ws * 0.514444 * sin(parametry3$wd * pi / 180), 2)[(parametry3$altitude - parametry3$altitude[1]) >= 0 & (parametry3$altitude - parametry3$altitude[1]) <= 500])
     vSRH = c(round(-parametry3$ws * 0.514444 * cos(parametry3$wd * pi / 180), 2)[(parametry3$altitude - parametry3$altitude[1]) >= 0 & (parametry3$altitude - parametry3$altitude[1]) <= 500])
-    for (i in 1:length(uSRH)) {
-      polygon(c(RM_x, uSRH[i:(i + 1)]),
-        c(RM_y, vSRH[i:(i + 1)]),
-        col = "red", border = NA
-      )
-    }
-  }
-
-  if (SRH_polygon == "36km_RM") {
-    parametry2 = sounding_compute(seq(1000, 100, length.out = length(wd)), altitude, seq(30, -60, length.out = length(wd)), seq(20, -40, length.out = length(wd)), wd, ws, storm_motion = storm_motion, accuracy = 3)
-    parametry3 = sounding_export(seq(1000, 100, length.out = length(wd)), altitude, seq(30, -60, length.out = length(wd)), seq(20, -40, length.out = length(wd)), wd, ws, storm_motion = storm_motion)
-    RM_y = round(-parametry2[which(names(parametry2) == "Bunkers_RM_M")] * cos(parametry2[which(names(parametry2) == "Bunkers_RM_A")] * pi / 180), 2)
-    RM_x = round(-parametry2[which(names(parametry2) == "Bunkers_RM_M")] * sin(parametry2[which(names(parametry2) == "Bunkers_RM_A")] * pi / 180), 2) # LM_x = m_x1 - 7.5 * (sqrt( 1 / (1+slope2*slope2)))
-    uSRH = c(round(-parametry3$ws * 0.514444 * sin(parametry3$wd * pi / 180), 2)[(parametry3$altitude - parametry3$altitude[1]) >= 3000 & (parametry3$altitude - parametry3$altitude[1]) <= 6000])
-    vSRH = c(round(-parametry3$ws * 0.514444 * cos(parametry3$wd * pi / 180), 2)[(parametry3$altitude - parametry3$altitude[1]) >= 3000 & (parametry3$altitude - parametry3$altitude[1]) <= 6000])
     for (i in 1:length(uSRH)) {
       polygon(c(RM_x, uSRH[i:(i + 1)]),
         c(RM_y, vSRH[i:(i + 1)]),
@@ -180,21 +144,6 @@ sounding_hodograph = function(ws, wd, altitude, max_hght = 12000, max_speed = 25
     }
   }
 
-  if (SRH_polygon == "36km_LM") {
-    parametry2 = sounding_compute(seq(1000, 100, length.out = length(wd)), altitude, seq(30, -60, length.out = length(wd)), seq(20, -40, length.out = length(wd)), wd, ws, storm_motion = storm_motion, accuracy = 3)
-    parametry3 = sounding_export(seq(1000, 100, length.out = length(wd)), altitude, seq(30, -60, length.out = length(wd)), seq(20, -40, length.out = length(wd)), wd, ws, storm_motion = storm_motion)
-    LM_y = round(-parametry2[which(names(parametry2) == "Bunkers_LM_M")] * cos(parametry2[which(names(parametry2) == "Bunkers_LM_A")] * pi / 180), 2)
-    LM_x = round(-parametry2[which(names(parametry2) == "Bunkers_LM_M")] * sin(parametry2[which(names(parametry2) == "Bunkers_LM_A")] * pi / 180), 2) # LM_x = m_x1 - 7.5 * (sqrt( 1 / (1+slope2*slope2)))
-    uSRH = c(round(-parametry3$ws * 0.514444 * sin(parametry3$wd * pi / 180), 2)[(parametry3$altitude - parametry3$altitude[1]) >= 3000 & (parametry3$altitude - parametry3$altitude[1]) <= 6000])
-    vSRH = c(round(-parametry3$ws * 0.514444 * cos(parametry3$wd * pi / 180), 2)[(parametry3$altitude - parametry3$altitude[1]) >= 3000 & (parametry3$altitude - parametry3$altitude[1]) <= 6000])
-    for (i in 1:length(uSRH)) {
-      polygon(c(LM_x, uSRH[i:(i + 1)]),
-        c(LM_y, vSRH[i:(i + 1)]),
-        col = "red", border = NA
-      )
-    }
-  }
-
   rect(min(xlm - 100), min(xlm - 100), max(xlm + 100), max(xlm + 100), col = rgb(1, 1, 1, alpha = 0.75), border = F)
 
   # abline(h = -20:20*5, lty = 2, col = "gray60")
@@ -223,35 +172,35 @@ sounding_hodograph = function(ws, wd, altitude, max_hght = 12000, max_speed = 25
   # find surface level
   sfc = floor(altitude / 1000)[1]
 
-  ux = approx(x = altitude, y = u, xout = seq(from = sfc, to = 1000, by = 50))$y
-  uy = approx(x = altitude, y = v, xout = seq(from = sfc, to = 1000, by = 50))$y
+  ux = approx(x = altitude, y = u, xout = seq(from = sfc, to = 500, by = 50))$y
+  uy = approx(x = altitude, y = v, xout = seq(from = sfc, to = 500, by = 50))$y
   ux[1] = u[!is.na(u)][1]
   uy[1] = v[!is.na(v)][1]
   lines(ux, uy, lwd = 3, col = "magenta")
 
-  ux = approx(x = altitude, y = u, xout = seq(from = 1000, to = 3000, by = 50))$y
-  uy = approx(x = altitude, y = v, xout = seq(from = 1000, to = 3000, by = 50))$y
+  ux = approx(x = altitude, y = u, xout = seq(from = 500, to = 1000, by = 50))$y
+  uy = approx(x = altitude, y = v, xout = seq(from = 500, to = 1000, by = 50))$y
   # ux[1] = u[!is.na(u)][1]
   # uy[1] = v[!is.na(v)][1]
   lines(ux, uy, lwd = 3, col = "red")
 
-  ux = approx(x = altitude, y = u, xout = seq(from = 3000, to = 9000, by = 50))$y
-  uy = approx(x = altitude, y = v, xout = seq(from = 3000, to = 9000, by = 50))$y
+  ux = approx(x = altitude, y = u, xout = seq(from = 1000, to = 1500, by = 50))$y
+  uy = approx(x = altitude, y = v, xout = seq(from = 1000, to = 1500, by = 50))$y
   # ux[1] = u[!is.na(u)][1]
   # uy[1] = v[!is.na(v)][1]
   lines(ux, uy, lwd = 3, col = "orange")
 
-  ux = approx(x = altitude, y = u, xout = seq(from = 6000, to = 9000, by = 50))$y
-  uy = approx(x = altitude, y = v, xout = seq(from = 6000, to = 9000, by = 50))$y
+  ux = approx(x = altitude, y = u, xout = seq(from = 1500, to = 2000, by = 50))$y
+  uy = approx(x = altitude, y = v, xout = seq(from = 1500, to = 2000, by = 50))$y
   # ux[1] = u[!is.na(u)][1]
   # uy[1] = v[!is.na(v)][1]
   lines(ux, uy, lwd = 3, col = "yellow")
 
-  ux = approx(x = altitude, y = u, xout = seq(from = 9000, to = 12000, by = 50))$y
-  uy = approx(x = altitude, y = v, xout = seq(from = 9000, to = 12000, by = 50))$y
+  #ux = approx(x = altitude, y = u, xout = seq(from = 9000, to = 12000, by = 50))$y
+  #uy = approx(x = altitude, y = v, xout = seq(from = 9000, to = 12000, by = 50))$y
   # ux[1] = u[!is.na(u)][1]
   # uy[1] = v[!is.na(v)][1]
-  lines(ux, uy, lwd = 3, col = "lightblue")
+  #lines(ux, uy, lwd = 3, col = "lightblue")
 
   # ux = approx(x = altitude, y = u, xout = seq(from = 12000, to = 16000, by = 50))$y
   # uy = approx(x = altitude, y = v, xout = seq(from = 12000, to = 16000, by = 50))$y
